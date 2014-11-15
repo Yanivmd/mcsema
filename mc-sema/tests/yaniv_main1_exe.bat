84ae4a11187301e7f068f36bcd3c18c71071392d
Yaniv <yaniv.m.david@gmail.com>
2014-11-15 18:42:44 +0200

    fixed with amir's bat

--- mc-sema/tests/yaniv_main1_exe.bat
+++ mc-sema/tests/yaniv_main1_exe.bat
@@ -1,4 +1,4 @@
-echo off
+@echo off
 
 call env.bat
 
@@ -7,36 +7,35 @@ set ext=exe
 rem set ext=obj
 
 del /q %filename%.cfg %filename%_opt.obj 
-
+cl /nologo /c logMain1.c
 rem %NASM_PATH%\nasm.exe -f win32 -o demo_test1.obj demo_test1.asm 
+
+
 
 rem if exist "%IDA_PATH%\idaq.exe" (
 rem    echo Using IDA to recover CFG
 rem    %BIN_DESCEND_PATH%\bin_descend_wrapper.py  -d -i=%filename%.%ext%  -entry-symbol=_main 
-rem	rem %BIN_DESCEND_PATH%\bin_descend_wrapper.py -b -d -func-map="%STD_DEFS%" -entry-symbol=_doCommand1 -i=%filename%.exe
-rem )
+	rem %BIN_DESCEND_PATH%\bin_descend_wrapper.py -b -d -func-map="%STD_DEFS%" -entry-symbol=_doCommand1 -i=%filename%.obj
 	
+	
+	rem -driver=_doCommand1,raw,return,C
+
+	rem -driver=start,raw,return,C
+	
+	rem %LLVM_PATH%\opt.exe -O3 -o %filename%_opt.bc %filename%.bc
+	
+	rem echo "NOW"
+	
+	rem %LLVM_PATH%\llvm-dis.exe %filename%_opt.bc
+	rem %LLVM_PATH%\llvm-dis.exe %filename%.bc
+	
+	rem %LLVM_PATH%\llc.exe  -filetype=obj %filename%_opt.bc
+	rem cl /Zi /nologo /ENTRY _driver__main %filename%_opt.obj
+	rem demo_driver1.exe
 rem ) else (
 	echo Using bin_descend to recover CFG
-	echo %BIN_DESCEND_PATH%
-    %BIN_DESCEND_PATH%\bin_descend.exe -d -l -entry-symbol=_main -i=%filename%.%ext%
-    echo "haaaaa"
+    %BIN_DESCEND_PATH%\bin_descend.exe -d -l -entry-symbol=_main -i=%filename%.obj
 	rem exit
 rem )
 
-rem %CFG_TO_BC_PATH%\cfg_to_bc.exe -i  %filename%.cfg  -o %filename%.bc  -ignore-unsupported     
-rem -driver=_doCommand1,raw,return,C
-
-rem -driver=start,raw,return,C
-
-rem %LLVM_PATH%\opt.exe -O3 -o %filename%_opt.bc %filename%.bc
-
-rem echo "NOW"
-
-rem %LLVM_PATH%\llvm-dis.exe %filename%_opt.bc
-rem %LLVM_PATH%\llvm-dis.exe %filename%.bc
-
-rem %LLVM_PATH%\llc.exe  -filetype=obj %filename%_opt.bc
-rem cl /Zi /nologo /ENTRY _driver__main %filename%_opt.obj
-rem demo_driver1.exe
-
+%CFG_TO_BC_PATH%\cfg_to_bc.exe -i %filename%.cfg -driver=test_entry,_main,0,return,C -o %filename%.bc  -ignore-unsupported     
